c81ed8a5988d7ff9d247cb434381ac5729498b2e
Sanved <sanved77@gmail.com>
2018-04-10 15:05:25 +0530

    init

---
+++ app/src/main/java/com/sanved/slotbookingapp/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.sanved.slotbookingapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_screen);
+
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent i = new Intent(SplashScreen.this, SplashScreen.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+}

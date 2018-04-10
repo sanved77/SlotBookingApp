2342121edb3a3a7c1f2a16d29a71093564be8482
Sanved <sanved77@gmail.com>
2018-04-10 15:38:26 +0530

    init

--- app/src/main/java/com/sanved/slotbookingapp/Login.java
+++ app/src/main/java/com/sanved/slotbookingapp/Login.java
@@ -4,12 +4,25 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 
 /**
  * Created by Sanved on 10-04-2018.
@@ -20,6 +33,7 @@ public class Login extends AppCompatActivity {
     EditText user, pass;
     Button add;
     ImageButton back;
+    ArrayList<UserDat> list;
 
     String struser, strpass;
     boolean isEdit = false;
@@ -34,7 +48,7 @@ public class Login extends AppCompatActivity {
 
     }
 
-    public void initVals(){
+    public void initVals() {
 
         // Just declaration and initialisation of the variables.
 
@@ -54,17 +68,57 @@ public class Login extends AppCompatActivity {
                 // Validations
 
                 // If the user is empty
-                if(user.getText().toString() == null){
+                if (user.getText().toString() == null) {
                     Toast.makeText(Login.this, "Please enter the username", Toast.LENGTH_SHORT).show();
                 }
                 // If the pass is empty
-                else if(pass.getText().toString() == null){
+                else if (pass.getText().toString() == null) {
                     Toast.makeText(Login.this, "Please enter the password", Toast.LENGTH_SHORT).show();
-                }
+                } else {
 
-                else{
+                    String url = "http://tapkeer.com/book/user.php";
 
-                    // TODO: 10-04-2018 Authentication
+                    list = new ArrayList<>();
+
+                    StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                            new Response.Listener<String>() {
+                                @Override
+                                public void onResponse(String response) {
+                                    try {
+                                        //converting the string to json array object
+                                        JSONArray array = new JSONArray(response);
+                                        String usertemp, passtemp;
+                                        //traversing through all the object
+                                        for (int i = 0; i < array.length(); i++) {
+
+                                            //getting product object from json array
+                                            JSONObject product = array.getJSONObject(i);
+
+
+                                            usertemp = product.getString("user");
+                                            passtemp = product.getString("pass");
+                                            Log.d("user - " + usertemp, "pass - " + passtemp);
+
+                                            list.add(new UserDat(usertemp, passtemp));
+
+                                        }
+
+                                        checkAndValidate(user.getText().toString(), pass.getText().toString());
+
+                                    } catch (JSONException e) {
+                                        e.printStackTrace();
+                                    }
+                                }
+                            },
+                            new Response.ErrorListener() {
+                                @Override
+                                public void onErrorResponse(VolleyError error) {
+
+                                }
+                            });
+
+                    //adding our stringrequest to queue
+                    Volley.newRequestQueue(Login.this).add(stringRequest);
 
                 }
 
@@ -80,5 +134,21 @@ public class Login extends AppCompatActivity {
         });
     }
 
+    public void checkAndValidate(String usert, String passt){
+
+        int flag = 0;
+
+        for(int i = 0; i < list.size(); i++){
+            if(usert.equals(list.get(i).user) && passt.equals(list.get(i).pass)){
+                flag = 1;
+                // TODO: 10-04-2018 Send to booking screen 
+            }
+        }
+
+        if(flag == 0){
+            Toast.makeText(this, "Login Failed, please check username and pass again", Toast.LENGTH_SHORT).show();
+        }
+
+    }
 
 }
---
+++ app/src/main/java/com/sanved/slotbookingapp/UserDat.java
@@ -0,0 +1,16 @@
+package com.sanved.slotbookingapp;
+
+/**
+ * Created by Sanved on 10-04-2018.
+ */
+
+public class UserDat {
+
+    String user,pass;
+
+    UserDat(String user, String pass){
+        this.user = user;
+        this.pass = pass;
+    }
+
+}

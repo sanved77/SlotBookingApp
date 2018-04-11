c8990e0bf4b910fe3ad1a559d8f49c9791ee2e16
Sanved <sanved77@gmail.com>
2018-04-11 11:37:00 +0530

    init

--- app/src/main/java/com/sanved/slotbookingapp/SlotBook.java
+++ app/src/main/java/com/sanved/slotbookingapp/SlotBook.java
@@ -1,10 +1,14 @@
 package com.sanved.slotbookingapp;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.wdullaer.materialdatetimepicker.date.DatePickerDialog;
@@ -15,11 +19,13 @@ import java.util.Calendar;
  * Created by Sanved on 10-04-2018.
  */
 
-public class SlotBook extends AppCompatActivity implements View.OnClickListener, com.wdullaer.materialdatetimepicker.date.DatePickerDialog.OnDateSetListener{
+public class SlotBook extends AppCompatActivity implements View.OnClickListener, com.wdullaer.materialdatetimepicker.date.DatePickerDialog.OnDateSetListener {
 
-    TextView date,slot;
-    Button setdate, setslot;
-    static int day,month,year;
+    TextView date, slot;
+    Button setdate, setslot, check;
+    static int day, month, year;
+    int slotnum;
+
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -28,19 +34,23 @@ public class SlotBook extends AppCompatActivity implements View.OnClickListener,
 
         date = findViewById(R.id.tvDate);
         slot = findViewById(R.id.tvSlot);
+        slotnum = 1;
 
         setdate = findViewById(R.id.bDate);
         setslot = findViewById(R.id.bSlot);
+        check = findViewById(R.id.bCheck);
 
         setdate.setOnClickListener(this);
         setslot.setOnClickListener(this);
+        check.setOnClickListener(this);
+
 
     }
 
     @Override
     public void onClick(View view) {
 
-        switch(view.getId()){
+        switch (view.getId()) {
             case R.id.bDate:
                 Calendar now = Calendar.getInstance();
                 DatePickerDialog dpd = DatePickerDialog.newInstance(
@@ -54,6 +64,48 @@ public class SlotBook extends AppCompatActivity implements View.OnClickListener,
 
             case R.id.bSlot:
 
+
+                AlertDialog.Builder build = new AlertDialog.Builder(SlotBook.this);
+
+                LayoutInflater inflater = this.getLayoutInflater();
+                final View dialogView = inflater.inflate(R.layout.slot_select, null);
+
+                final RadioGroup radioSexGroup = dialogView.findViewById(R.id.radioSex);
+
+                build
+                        .setTitle("Select Slot")
+                        .setView(dialogView)
+                        .setMessage("Select one of the three slots")
+                        .setPositiveButton("Select", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                int selectedId = radioSexGroup.getCheckedRadioButtonId();
+
+                                switch (selectedId) {
+                                    case R.id.r1:
+                                        slotnum = 1;
+                                        break;
+                                    case R.id.r2:
+                                        slotnum = 2;
+                                        break;
+                                    case R.id.r3:
+                                        slotnum = 3;
+                                        break;
+                                }
+                            }
+                        })
+                        .setCancelable(false);
+
+                build.create().show();
+
+                slot.setText("Slot - " + slotnum);
+
+                break;
+
+            case R.id.bCheck:
+
+
+
                 break;
         }
 
@@ -64,5 +116,12 @@ public class SlotBook extends AppCompatActivity implements View.OnClickListener,
         day = dayOfMonth;
         month = monthOfYear;
         this.year = year;
+
+        date.setText(dayOfMonth + "/" + monthOfYear+ "/" + year);
+
     }
+
+
 }
+
+
